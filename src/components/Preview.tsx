573647c0a35ad6d1a3362ecacc6da188ac0a2fa5
Michael <michaelfarquhar4@gmail.com>
2022-05-23 11:27:30 -0400

    Display current svgs in preview for svg's selected

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,15 +1,30 @@
 import { Box, Card, Typography } from '@mui/material';
 import { FC } from 'react';
+import SVG from 'react-inlinesvg';
+import { useRecoilValue } from 'recoil';
+import { svgState } from '../App';
 
 interface Props {}
 
 export const Preview: FC<Props> = (props) => {
+    const svgs = useRecoilValue(svgState);
+    const svgDirectory = `${process.env.PUBLIC_URL}/svgs/`;
+
     return (
         <Card>
             <Box>
                 <Typography variant="h5" component="div" gutterBottom>
                     Preview
                 </Typography>
+                {svgs &&
+                    svgs.map((fileName: string) => (
+                        <SVG
+                            src={`${svgDirectory}${fileName}.svg`}
+                            width={70}
+                            height={70}
+                            title={fileName}
+                        />
+                    ))}
             </Box>
         </Card>
     );
